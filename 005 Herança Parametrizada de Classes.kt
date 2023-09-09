e4abbe0be74c8c1acc5902efccdd3c44ae7dfb00
leonardocassauara <leomaia.rj@hotmail.com>
2023-09-09 12:41:35 -0300

    feat: herança parametrizada

---
+++ 005 Herança Parametrizada de Classes.kt
@@ -0,0 +1,14 @@
+open class Tiger (val origin: String) {
+    fun sayHello() {
+        println("A tiger from ${origin} says: grrhh!")
+    }
+}
+
+
+class SiberianTiger: Tiger ("Siberia")
+
+
+fun main() {
+    val tiger: Tiger = SiberianTiger()
+    tiger.sayHello()
+}
